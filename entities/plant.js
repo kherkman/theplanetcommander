fa48018b063f07f6941f337b92bf9be03e7cc458
kherkman <kristian.herkman@gmail.com>
2025-07-12 14:22:48 +0300

    Delete entities directory

--- entities/plant.js
+++
@@ -1,8 +0,0 @@
-// entities/plant.js
-export function createPlant() {
-    // TODO: Create a 3D plant/tree model. A brown cylinder for the trunk and a green cone for the leaves is a simple start.
-    const geometry = new THREE.ConeGeometry(0.5, 1, 8);
-    const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
-    const plant = new THREE.Mesh(geometry, material);
-    return plant;
-}
--- entities/worker.js
+++
@@ -1,8 +0,0 @@
-// entities/worker.js
-export function createWorker() {
-    // A capsule is a good shape for a person/robot
-    const geometry = new THREE.CapsuleGeometry(0.2, 0.5, 4, 8); 
-    const material = new THREE.MeshPhongMaterial({ color: 0xff0000 }); // Bright red
-    const worker = new THREE.Mesh(geometry, material);
-    return worker;
-}
